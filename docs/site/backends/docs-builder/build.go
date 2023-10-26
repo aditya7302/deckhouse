56378bb0e8be53d3bfb5e65d7d3751c9052b691a
Daniil <daniil.stepanenko@flant.com>
2023-10-26 15:32:30 +0300

    WIP

---
+++ docs/site/backends/docs-builder/build.go
@@ -0,0 +1,68 @@
+// Copyright 2023 Flant JSC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package main
+
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/flant/docs_builder/pkg/hugo"
+	"github.com/flant/docs_builder/pkg/k8s"
+
+	"k8s.io/klog/v2"
+)
+
+func newBuildHandler(src string, dst string, cmManager *k8s.ConfigmapManager) *buildHandler {
+	return &buildHandler{
+		src:       src,
+		dst:       dst,
+		cmManager: cmManager,
+	}
+}
+
+type buildHandler struct {
+	src       string
+	dst       string
+	cmManager *k8s.ConfigmapManager
+}
+
+func (b *buildHandler) ServeHTTP(writer http.ResponseWriter, request *http.Request) {
+	err := b.build()
+	if err != nil {
+		klog.Error(err)
+		http.Error(writer, "Internal server error", http.StatusInternalServerError)
+		return
+	}
+
+	writer.WriteHeader(http.StatusOK)
+}
+
+func (b *buildHandler) build() error {
+	flags := hugo.Flags{
+		//TODO: Quiet:  true,
+		Source: b.src,
+	}
+
+	err := hugo.Build(flags)
+	if err != nil {
+		return fmt.Errorf("hugo build: %w", err)
+	}
+
+	err = b.cmManager.Remove()
+	if err != nil {
+		return fmt.Errorf("remove cm: %w", err)
+	}
+	return nil
+}
--- docs/site/backends/docs-builder/main.go
+++ docs/site/backends/docs-builder/main.go
@@ -1,12 +1,88 @@
+// Copyright 2023 Flant JSC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 package main
 
 import (
-	"log"
+	"context"
+	"errors"
+	"flag"
+	"github.com/gorilla/mux"
 	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
+
+	"github.com/flant/docs_builder/pkg/k8s"
+
+	"k8s.io/klog/v2"
 )
 
+// flags
+var (
+	listenAddress string
+	src           string
+	dst           string
+)
+
+func init() {
+	flag.StringVar(&listenAddress, "address", ":8081", "Address to listen on")
+	flag.StringVar(&src, "src", "/tmp/src", "Directory to load source files")
+	flag.StringVar(&dst, "dst", "/tmp/dst", "Directory for compiled files")
+}
+
 func main() {
-	http.HandleFunc("/healthz", func(w http.ResponseWriter, r *http.Request) { _, _ = w.Write([]byte("ok")) })
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) { _, _ = w.Write([]byte("hello world")) })
-	log.Fatal(http.ListenAndServe(":8081", nil))
+	flag.Parse()
+
+	ctx, stopNotify := signal.NotifyContext(context.Background(), os.Interrupt, syscall.SIGINT, syscall.SIGTERM)
+	defer stopNotify()
+
+	cmManager, err := k8s.NewConfigmapManager()
+	if err != nil {
+		klog.Fatalf("new cm manager: %s", err)
+	}
+
+	r := mux.NewRouter()
+	r.HandleFunc("/healthz", func(w http.ResponseWriter, r *http.Request) { _, _ = w.Write([]byte("ok")) })
+	r.Handle("/loadDocArchive/{moduleName}/{version}", newLoadHandler(src)).Methods(http.MethodPost)
+	r.Handle("/build", newBuildHandler(src, dst, cmManager)).Methods(http.MethodPost)
+
+	srv := &http.Server{
+		Addr:    listenAddress,
+		Handler: r,
+	}
+
+	go func() {
+		if err := srv.ListenAndServe(); err != nil && !errors.Is(err, http.ErrServerClosed) {
+			klog.Fatalf("listen: %s", err)
+		}
+	}()
+	klog.Info("Server Started")
+
+	err = cmManager.Create(ctx)
+	if err != nil {
+		klog.Fatalf("create sync config map: %s", err)
+	}
+
+	<-ctx.Done()
+	klog.Info("Server Stopped")
+
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+
+	if err := srv.Shutdown(ctx); err != nil {
+		klog.Fatalf("Server Shutdown Failed:%+v", err)
+	}
 }
